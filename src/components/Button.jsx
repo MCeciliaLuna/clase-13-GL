931ddac770ea7117c23066039abaec86acbc4688
MCeciliaLuna <lunama.cecilia@live.com>
2023-07-03 18:31:54 -0300

    creación de carpeta componentes con button

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import '@styles/Button.css'; //dondequiera que esté la carpeta "styles", buscala y buscá button.css
+
+const Button = () => {
+  return (
+    <button className="alias">
+      Alias import Vite
+    </button>
+  );
+};
+
+export default Button;
